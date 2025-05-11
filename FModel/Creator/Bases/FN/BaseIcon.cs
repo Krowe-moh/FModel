c8de28130c52023d94884895dd91c64c7b16cd01
AllyJaxx <jaxmh2005@gmail.com>
2025-05-11 13:51:11 -0400

    switch

--- FModel/Creator/Bases/FN/BaseIcon.cs
+++ FModel/Creator/Bases/FN/BaseIcon.cs
@@ -257,28 +257,19 @@ public class BaseIcon : UCreator
 
         var season = Utils.GetLocalizedResource("AthenaSeasonItemDefinitionInternal", "SeasonTextFormat", "Season {0}");
         var introduced = Utils.GetLocalizedResource("Fort.Cosmetics", "CosmeticItemDescription_Season", "\nIntroduced in <SeasonText>{0}</>.");
-        if (s == "10")
-        {
-            return Utils.RemoveHtmlTags(string.Format(introduced, string.Format(season, "X")));
-        }
+        if (s == "10") return Utils.RemoveHtmlTags(string.Format(introduced, string.Format(season, "X")));
         if (initial <= 10) return Utils.RemoveHtmlTags(string.Format(introduced, string.Format(season, s)));
 
         var chapter = Utils.GetLocalizedResource("AthenaSeasonItemDefinitionInternal", "ChapterTextFormat", "Chapter {0}");
         var chapterFormat = Utils.GetLocalizedResource("AthenaSeasonItemDefinitionInternal", "ChapterSeasonTextFormat", "{0}, {1}");
         var d = string.Format(chapterFormat, string.Format(chapter, chapterIdx), string.Format(season, seasonIdx));
-        if (s == "27")
+        return s switch
         {
-            return Utils.RemoveHtmlTags(string.Format(introduced, string.Format(chapterFormat, string.Format(chapter, chapterIdx), string.Format(season, "OG"))));
-        }
-        if (s == "32")
-        {
-            return Utils.RemoveHtmlTags(string.Format(introduced, string.Format(chapterFormat, string.Format(chapter, chapterIdx), string.Format(season, "Remix"))));
-        }
-        if (s == "35")
-        {
-            return Utils.RemoveHtmlTags(string.Format(introduced, string.Format(chapterFormat, string.Format(chapter, chapterIdx), string.Format(season, "MS1"))));
-        }
-        return Utils.RemoveHtmlTags(string.Format(introduced, d));
+            "27" => Utils.RemoveHtmlTags(string.Format(introduced, string.Format("Fortnite: OG"))),
+            "32" => Utils.RemoveHtmlTags(string.Format(introduced, string.Format("Fortnite: Remix"))),
+            "35" => Utils.RemoveHtmlTags(string.Format(introduced, string.Format(chapterFormat, string.Format(chapter, chapterIdx), string.Format("MS1")))),
+            _ => Utils.RemoveHtmlTags(string.Format(introduced, d))
+        };
     }
 
     protected void CheckGameplayTags(FInstancedStruct[] dataList)
